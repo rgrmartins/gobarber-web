20ad01adff7c1e9455ca0096f78257f47003f9fe
Roger Martins <rogermartinsdesign@gmail.com>
2019-12-28 21:30:16 -0400

    Implementando e Finalizando GoBarber Web

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import Notifications from '~/components/Notifications';
 
 import logo from '~/assets/logo-sidebar.svg';
-
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -21,11 +23,14 @@ export default function Header() {
 
           <Profile>
             <div>
-              <strong>Roger Martins</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu Perfil</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
               alt="Roger Martins"
             />
           </Profile>
